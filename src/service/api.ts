0a6070268a1e4a17533c1f0e931ed0575cdc441d
Danilo Schleu <daniloschleu@gmail.com>
2023-10-07 14:02:25 -0300

    fix: url api, change md5 lib

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
-import * as md5 from "md5";
+
+import { Md5 } from "ts-md5";
 
 const baseURl = process.env.VITE_APP_MARVEL_URL || "";
 const apiKey = process.env.VITE_APP_MARVEL_PUBLIC_KEY || "";
 const apiPrivateKey = process.env.VITE_APP_MARVEL_PRIVATE_KEY || "";
 const ts = 1;
-const hash = md5(ts + apiPrivateKey + apiKey);
+const hash = Md5.hashStr(ts + apiPrivateKey + apiKey);
 
 export const Axios = axios.create({
   baseURL: baseURl,
